eba36835f8cb4385c482b7b83791aa415c3dbde0
Roubert Edgar <roubertedgar@gmail.com>
2020-04-05 16:30:05 -0300

    Studing android canvas tools, drowing lines on a edit text

--- app/src/main/java/com/downstairs/linededittext/LinedEditText.kt
+++ app/src/main/java/com/downstairs/linededittext/LinedEditText.kt
@@ -11,35 +11,29 @@ import androidx.appcompat.widget.AppCompatTextView
 import kotlin.math.roundToInt
 
 
-class LinedEditText(context: Context, attributes: AttributeSet) : AppCompatTextView(context, attributes) {
-
-    private val linePath = Path()
-    private val linePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-
-    private val textPaint = TextPaint(TextPaint.ANTI_ALIAS_FLAG)
+class LinedEditText @JvmOverloads constructor(
+    context: Context,
+    attributes: AttributeSet? = null,
+    styleAttribute: Int = 0
+) : AppCompatTextView(context, attributes, styleAttribute) {
 
     companion object {
-        const val MIN_LAYOUT_SIZE_DP = 50f
         const val INITIAL_X = 0
+        const val MIN_LAYOUT_SIZE_DP = 50f
         const val TEXT_LENGTH = 6
     }
+
+    //Line
+    private val lines = FloatArray(TEXT_LENGTH * 4)
+    private val linePaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     init {
         linePaint.style = Paint.Style.STROKE
         linePaint.strokeWidth = dpToPixel(2f)
         linePaint.color = Color.GRAY
-
-        configureText()
-    }
-
-    private fun configureText() {
-        textPaint.color = Color.BLACK
-        textPaint.textSize = dpToPixel(28f)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val fontMetrics = textPaint.fontMetrics
-
         val desiredHeight =
             (dpToPixel(MIN_LAYOUT_SIZE_DP) + paddingTop + paddingBottom).roundToInt()
         val desiredWidth = MeasureSpec.getSize(widthMeasureSpec) + paddingLeft + paddingRight
@@ -50,51 +44,36 @@ class LinedEditText(context: Context, attributes: AttributeSet) : AppCompatTextV
         setMeasuredDimension(measuredWidth, measuredHeight)
     }
 
-
-    override fun onDraw(canvas: Canvas?) {
-        canvas?.also {
-            drawPath(it)
-            drawText(it)
-        }
+    override fun onSizeChanged(width: Int, height: Int, oldWidht: Int, oldHeight: Int) {
+        val availableHeight = height - paddingTop - paddingBottom
+        createBottomLines(width, height)
     }
 
-    private fun drawText(canvas: Canvas) {
-        canvas.drawText("A", getPathLineX().toFloat(), getPathLineY().toFloat(), textPaint)
-    }
-
-    private fun drawPath(canvas: Canvas) {
-        var pathX = getPathLineX().toFloat()
-        val pathY = getPathLineY().toFloat()
-        val drawableWidth = getDrawableWidth()
-
-        val lineSpacing = 20
-        val lineSize = measureLineSize(drawableWidth, lineSpacing)
-
-        for (i in 1..TEXT_LENGTH) {
-            linePath.moveTo(pathX + lineSpacing, pathY)
-            pathX += lineSize
-
-            linePath.lineTo(pathX, pathY)
-            pathX += lineSpacing
-        }
-
-        canvas.drawPath(linePath, linePaint)
-    }
-
-    private fun measureLineSize(drawableWidth: Int, lineSpacing: Int): Float {
-        val maxLineSize = drawableWidth / TEXT_LENGTH
-        val remainWidth = drawableWidth % TEXT_LENGTH
+    private fun createBottomLines(width: Int, height: Int) {
+        val availableWidth = width - paddingStart - paddingEnd
+        val availableLineSize = availableWidth / TEXT_LENGTH
+        val remainWidth = availableWidth % TEXT_LENGTH
         val minLineSpace = remainWidth.toFloat() / (TEXT_LENGTH - 1).toFloat()
 
-        return maxLineSize - (lineSpacing - minLineSpace)
+        val lineSpacing = 20 + minLineSpace / 6
+        val lineSize = availableLineSize - lineSpacing
+
+        val yPoint = (height - paddingBottom).toFloat() - dpToPixel(2f)
+        var xPoint = INITIAL_X + paddingStart + lineSpacing / 2
+
+        for (i in lines.indices step 4) {
+            lines[i] = xPoint
+            lines[i + 1] = yPoint
+            lines[i + 2] = xPoint + lineSize
+            lines[i + 3] = yPoint
+
+            xPoint += lineSize + lineSpacing
+        }
     }
 
-    private fun getCharWidth() = textPaint.measureText("A", 0, 1)
-
-    private fun getPathLineX(): Int = INITIAL_X + paddingStart
-
-    private fun getPathLineY(): Int = height - paddingBottom - 20
-
-    private fun getDrawableWidth(): Int = width - paddingStart - paddingEnd
-
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.run {
+            drawLines(lines, linePaint)
+        }
+    }
 }
